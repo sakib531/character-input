e0ef489975a33ae7bb2e38c64bdccdf6cce9b4c9
sakib531 <142710106+sakib531@users.noreply.github.com>
2024-02-23 23:07:44 +0600

    Add files via upload

---
+++ project-15.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char a = 'R';
+    char b = 'A';
+    char c = 'F';
+    char d = 'I';
+    char e = 'D';
+    printf("\n\t%c",a);
+    printf("%c",b);
+    printf("%c",c);
+    printf("%c",d);
+    printf("%c",e);
+    return 0;
+}
---
+++ project-6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char country1 = 'B';
+    char country2 = 'a';
+    char country3 = 'n';
+    char country4 = 'g';
+    char country5 = 'l';
+    char country6 = 'a';
+    char country7 = 'd';
+    char country8 = 'e';
+    char country9 = 's';
+    char country10 = 'h';
+    printf("\n\t%c",country1);
+    printf("%c",country2);
+    printf("%c",country3);
+    printf("%c",country4);
+    printf("%c",country5);
+    printf("%c",country6);
+    printf("%c",country7);
+    printf("%c",country8);
+    printf("%c",country9);
+    printf("%c\n\t",country10);
+    return 0;
+}
+
